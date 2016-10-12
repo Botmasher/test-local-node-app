4f9310a63a9795956d5cd287c98f526c36a8d5cf
Botmasher <languagejosh@hotmail.com>
2016-10-12 11:49:17 -0400

    Add and require test module in server code

--- nodeserver.js
+++ nodeserver.js
@@ -1,6 +1,9 @@
-// simple server
-// run from terminal: node nodeserver.js
+// server run from terminal: node nodeserver.js
 const http = require('http');
+const url = require('url');
+
+// import .js files
+var testmodule = require('./testmodule');
 
 const host = '127.0.0.1';
 const port = 8080;
@@ -9,15 +12,17 @@ const server = http.createServer(function(req, res){
 	res.statusCode = 200;
 	res.setHeader('Content-Type', 'text/plain');
 
+	testmodule.init();
+
 	res.writeHead(200, {"Content-Type": "text/plain"});
 	// parse and use url
 	var uri = url.parse(req.url).pathname;
 	if (uri == '/') {
-		res.write('Homepage');
+		pageTxt = 'Homepage\n';
 	} else if (uri == '/subdir') {
-		res.write('One level down');
+		pageTxt = 'One level down';
 	}
-
+	res.write (pageTxt);
 	res.end('Hello World\n');
 });
 
@@ -27,11 +32,13 @@ server.listen(port, host, function() {
 
 // events
 server.on ('connect', function() {
-	document.write("Hello World!!!");
+	console.log ('Welcome!');
 });
 
 server.on ('close', function() {
 	console.log ('Bye for now!');
 });
 
-// https://openclassrooms.com/courses/ultra-fast-applications-using-node-js/node-js-modules-and-npm
+/*
+https://openclassrooms.com/courses/ultra-fast-applications-using-node-js/node-js-modules-and-npm
+*/
---
+++ testmodule.js
@@ -0,0 +1,9 @@
+exports.init = function() {
+	writeDoc();
+}
+
+function writeDoc () {
+	console.log("Got to inner module!");
+	//var body = document.getElementById('body');
+	//body.innerHTML('Hello, World!');
+}
