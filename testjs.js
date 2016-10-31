511d33136b9bdd144c9c50e51e60dd9f12f4dc43
Botmasher <languagejosh@hotmail.com>
2016-10-31 00:22:36 -0400

    Add db connection to simple callback data test

--- testjs.js
+++ testjs.js
@@ -1,3 +1,10 @@
+// postgres db connection
+var pg = require('pg');
+var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/orangutan';
+var db = new pg.Client(connectionString);
+db.connect();
+
+
 // simple server - run from terminal: node filename.js
 var http = require('http');
 var url = require('url');
@@ -5,25 +12,22 @@ var url = require('url');
 const host = '127.0.0.1';
 const port = 8080;
 
+// server headers and routes
 const server = http.createServer(function(req, res){
 	res.statusCode = 200;
 	res.setHeader('Content-Type', 'text/plain');
-
 	res.writeHead(200, {"Content-Type": "text/plain"});
+	
 	// parse and use url
 	var uri = url.parse(req.url).pathname;
-	
 	if (uri == '/') {
-		
 		// test passing in function
-		getOutput (function(data) {
+		getDatabaseOutput (db, 'SELECT * FROM location', function (data) {
 			for (index in data.rows) {
 				console.log (data.rows[index].name);
 			}
 		});
-
 	}
-
 	res.write ("See console for output.");
 	res.end();
 });
@@ -43,9 +47,30 @@ server.on ('close', function() {
 
 
 
-// grab data and run passed-in callback
-function getOutput (callback) {
+// grab table data and run passed-in callback
+function getDatabaseOutput (client, query, callback) {
+	/*
+	 * 	@param {pg.Client} client 	 -  the database to query
+	 *	@param {String}    query 	 -  the sql query to run
+	 * 	@param {Function}  callback  -  the method to run on query end
+	 */
+ 
+	// run query then call your method
+	client.query (query, function (error, output) {
+		
+		if (error) throw error;
 
+		client.end (function (error) {
+			if (error) throw error;
+			client.end ();	
+			callback (output);
+		});
+	});
+}
+
+
+// grab fake data and run passed-in callback
+function getOutput (callback) {
 	// data to use in callback
 	var dogs = {
 		rows: [
@@ -63,6 +88,5 @@ function getOutput (callback) {
 			}
 		]
 	};
-
 	callback (dogs);			// run the user function
 }
