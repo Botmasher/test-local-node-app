9e966d78fbbc87420eaa96b42a9662b4195357a7
Botmasher <languagejosh@hotmail.com>
2016-11-12 11:37:45 -0500

    Allow app prototype methods to check for undefined optional params

--- nodeserver.js
+++ nodeserver.js
@@ -25,12 +25,18 @@ App.prototype.setViews = function (viewsDirectory, viewEngine) {
 App.prototype.setTemplate = function (template, templateVars) {
 	// set the template for the next view render
 	this.template = template;
-	this.templateVars = templateVars;
+	// template variables optional
+	if (templateVars === undefined) {
+		this.templateVars = {}
+	} else {
+		this.templateVars = templateVars;
+	}
 }
 App.prototype.get = function (route) {
 	// simple get using rendered template and passed in template variables object
 	var template = this.template;
 	var templateVars = this.templateVars;
+	// express app get and render
 	this.app.get (route, function (req, res) {
 		res.setHeader ('Content-Type', 'text/html');
 		res.render (template, templateVars);
@@ -39,14 +45,23 @@ App.prototype.get = function (route) {
 App.prototype.getWithData = function (route, query, queryArgs) {
 	/*
 	 * 	Define endpoint, query to run and template to render
-	 * 		- uses current app template and templateVars to render
+	 * 	  - uses current app template and templateVars to render
+	 * 	  - takes query and string interpolation args expected by pg query
 	 */
+	
 	// store template info for building template
 	var template = this.template;
 	var templateVars = this.templateVars;
+
+	// run the simple get method instead if there's no query
+	if (query === undefined) this.get (route);
+
 	// express app get method
 	this.app.get (route, function (req, res) {
 		res.setHeader('Content-Type', 'text/html');
+		// if query string args are not present set them to empty
+		if (queryArgs === undefined) queryArgs = [];
+		// my method for querying - pass callback to render once query is done
 		getDatabaseOutput (query, queryArgs, function (output) {
 			// add data to template variables
 			templateVars.data = output.rows;
@@ -57,10 +72,11 @@ App.prototype.getWithData = function (route, query, queryArgs) {
 }
 App.prototype.getJSON = function (route, query, qArgs) {
 	/*
-	 * 	Define endpoint and query for JSON
+	 * 	Define endpoint and query for JSON using basic express app get
 	 */
 	this.app.get (route, function (req, res) {
 		res.setHeader ('Content-Type', 'text/plain');
+		// my db querying method - pass callback to store and display output
 		getDatabaseOutput (query, qArgs, function (output) {
 			o = JSON.stringify (output.rows, null, "  ");
 			res.end (o);
@@ -72,12 +88,12 @@ App.prototype.getJSON = function (route, query, qArgs) {
 var myApp = new App();
 myApp.setViews('./views', 'ejs');
 
-// test database query load to page
-myApp.setTemplate ('jessica', {title:'xyz'});
-myApp.getWithData ('/test1/', 'SELECT * FROM location', []);
+// test database query load to page - "data" key added from query in getWithData
+myApp.setTemplate ('jessica', {title:'First Test'});
+myApp.getWithData ('/test1/', 'SELECT * FROM location');
 
 // test simple get
-myApp.setTemplate ('jessica', { title: 'test2', data: null });
+myApp.setTemplate ('jessica', { title: 'Second Test', data: null });
 myApp.get ('/test2/');
 
 myApp.listen (8080);
